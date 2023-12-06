36af8828473f23f1a330e60de833cf120143e131
haiseno <haise.zhao@gmail.com>
2023-12-05 20:16:21 -0800

    Update input validation behavior
    Prior to this change, users could only see validation if they unfocus
    the input; validation would go away when the user refocuses the input.
    This change maintains the previous behavior, while also adding live
    validation behavior if the user had interacted with the input prior.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
 const form = document.querySelector(".form");
+const inputs = document.querySelectorAll("input");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
+
+inputs.forEach(input => {
+  input.addEventListener("blur", () => { 
+    if (input.value) {
+      input.classList.add("interacted");
+    }
+    else {
+      input.classList.remove("interacted");
+    }
+  });
+});
 
 form.addEventListener("submit", isFormSubmittable);
 
